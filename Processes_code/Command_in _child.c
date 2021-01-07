9ef22c4d28861fb3906bbab3ebb2aebc4aaab889
99003159 <75420482+99003159@users.noreply.github.com>
2021-01-07 14:02:33 +0530

    Add files via upload

---
+++ Processes_code/Command_in _child.c
@@ -0,0 +1,27 @@
+#include<sys/wait.h>
+#include<stdio.h>
+#include<unistd.h>
+int main()
+{
+int ret,status;
+char cmd[10];
+ret=fork();
+if(ret<0)
+{
+perror("fork");
+}
+else if(ret==0)
+{
+printf("Enter the command you want to execute :");
+scanf("%s",cmd);
+execlp(cmd,cmd,NULL);
+exit(0);
+}
+else
+{
+waitpid(-1,&status,0);
+printf("The child has comleted\n");
+printf("The status is %d",WEXITSTATUS(status));
+}
+}
+
