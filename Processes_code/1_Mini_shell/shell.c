be8a73a81d9394c084c40b71065d1984b77b0f90
99003159 <75420482+99003159@users.noreply.github.com>
2021-01-10 12:07:26 +0530

    Update shell.c

--- Processes_code/1_Mini_shell/shell.c
+++ Processes_code/1_Mini_shell/shell.c
@@ -1,1 +1,24 @@
-
+#include"shell.h"
+void shell()
+{
+int ret,status;
+char cmd[10];
+ret=fork();
+if(ret<0)
+{
+perror("fork");
+}
+else if(ret==0)
+{
+printf("Enter the command you want to execute :");
+scanf("%s",cmd);
+execlp(cmd,cmd,NULL);
+exit(0);
+}
+else
+{
+waitpid(-1,&status,0);
+printf("The child has comleted\n");
+printf("The status is %d",WEXITSTATUS(status));
+}
+}
